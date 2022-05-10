2963abde1c8739c58cef365dec9b5fdcd0425808
Shane <shaneiadt@gmail.com>
2022-05-10 20:26:23 +0100

    build: update dist

--- dist/useWorker.d.ts
+++ dist/useWorker.d.ts
@@ -1,16 +1,14 @@
 export declare type Work = () => (this: Window, e: MessageEvent<any>) => void;
 export declare enum STATUS {
-  IDLE = "IDLE",
-  SUCCESS = "SUCCESS",
-  UNSUPPORTED = "UNSUPPORTED",
-  ERROR = "ERROR",
-  SENT = "SENT",
+    IDLE = "IDLE",
+    SUCCESS = "SUCCESS",
+    UNSUPPORTED = "UNSUPPORTED",
+    ERROR = "ERROR",
+    SENT = "SENT"
 }
-export declare const useWorker: <T extends string | number | object>(
-  action: () => void
-) => {
-  postMessage: (arg: T) => void;
-  result: string;
-  status: STATUS;
-  updateAction: (action: () => void) => void;
+export declare const useWorker: <T, K>(action: () => void) => {
+    postMessage: (arg: T) => void;
+    result: K | undefined;
+    status: STATUS;
+    updateAction: (userAction: () => void) => void;
 };
--- dist/useWorker.js
+++ dist/useWorker.js
@@ -1,53 +1,55 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.useWorker = exports.STATUS = void 0;
+/* eslint-disable no-constructor-return */
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
 const react_1 = require("react");
 var STATUS;
 (function (STATUS) {
-  STATUS["IDLE"] = "IDLE";
-  STATUS["SUCCESS"] = "SUCCESS";
-  STATUS["UNSUPPORTED"] = "UNSUPPORTED";
-  STATUS["ERROR"] = "ERROR";
-  STATUS["SENT"] = "SENT";
-})((STATUS = exports.STATUS || (exports.STATUS = {})));
+    STATUS["IDLE"] = "IDLE";
+    STATUS["SUCCESS"] = "SUCCESS";
+    STATUS["UNSUPPORTED"] = "UNSUPPORTED";
+    STATUS["ERROR"] = "ERROR";
+    STATUS["SENT"] = "SENT";
+})(STATUS = exports.STATUS || (exports.STATUS = {}));
 const fnToworkerURL = (fn) => {
-  var blob = new Blob(["(" + fn.toString() + ")()"], {
-    type: "text/javascript",
-  });
-  return URL.createObjectURL(blob);
+    const blob = new Blob([`(${fn.toString()})()`], {
+        type: "text/javascript",
+    });
+    return URL.createObjectURL(blob);
 };
 class WorkerBuilder extends Worker {
-  constructor(url) {
-    super(url);
-    return new Worker(url);
-  }
+    constructor(url) {
+        super(url);
+        return new Worker(url);
+    }
 }
 const useWorker = (action) => {
-  const [result, setResult] = (0, react_1.useState)("");
-  const [status, setStatus] = (0, react_1.useState)(STATUS.IDLE);
-  const [instance, setInstance] = (0, react_1.useState)(
-    new WorkerBuilder(fnToworkerURL(action))
-  );
-  (0, react_1.useEffect)(() => {
-    setInstance(() => new WorkerBuilder(fnToworkerURL(action)));
-  }, [action]);
-  instance.onmessage = ({ data }) => {
-    setStatus(STATUS.SUCCESS);
-    if (data) {
-      setResult(data);
-    }
-  };
-  instance.onerror = (e) => {
-    console.error(e);
-    setStatus(STATUS.ERROR);
-  };
-  const postMessage = (arg) => {
-    setStatus(STATUS.SENT);
-    instance.postMessage(arg);
-  };
-  const updateAction = (action) => {
-    setInstance(() => new WorkerBuilder(fnToworkerURL(action)));
-  };
-  return { postMessage, result, status, updateAction };
+    const [result, setResult] = (0, react_1.useState)();
+    const [status, setStatus] = (0, react_1.useState)(STATUS.IDLE);
+    const [instance, setInstance] = (0, react_1.useState)(new WorkerBuilder(fnToworkerURL(action)));
+    (0, react_1.useEffect)(() => {
+        setInstance(() => new WorkerBuilder(fnToworkerURL(action)));
+    }, [action]);
+    instance.onmessage = ({ data }) => {
+        setStatus(STATUS.SUCCESS);
+        if (data) {
+            setResult(data);
+        }
+    };
+    instance.onerror = (e) => {
+        // eslint-disable-next-line no-console
+        console.error(e);
+        setStatus(STATUS.ERROR);
+    };
+    const postMessage = (arg) => {
+        setStatus(STATUS.SENT);
+        instance.postMessage(arg);
+    };
+    const updateAction = (userAction) => {
+        setInstance(() => new WorkerBuilder(fnToworkerURL(userAction)));
+    };
+    return { postMessage, result, status, updateAction };
 };
 exports.useWorker = useWorker;
